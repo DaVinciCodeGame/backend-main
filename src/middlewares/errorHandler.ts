aca256e25e3e5d7f6f7506ea8f4c1e94d0c3f8b1
GogumaYummy <sora1211x@gmail.com>
2023-01-09 08:40:13 +0900

    feat: express 앱 생성

---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,14 @@
+import { ErrorRequestHandler } from 'express';
+import logger from '../config/logger';
+
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  const { stack } = err;
+  logger.error(stack);
+
+  if (err.isBoom)
+    res.status(err.output.statusCode).json({ errorMessage: err.message });
+  else if (err.isJoi) res.status(400).json({ errorMessage: err.message });
+  else res.status(500).json({ errorMessage: '알 수 없는 오류' });
+};
+
+export default errorHandler;
---
+++ src/middlewares/morgan.ts
@@ -0,0 +1,14 @@
+import morgan from 'morgan';
+import logger from '../config/logger';
+
+const stream: morgan.StreamOptions = {
+  write(message) {
+    logger.http(message);
+  },
+};
+const skip = () => process.env.NODE_ENV !== 'development';
+
+export default morgan(
+  process.env.NODE_ENV === 'production' ? 'combined' : 'dev',
+  { stream, skip }
+);
