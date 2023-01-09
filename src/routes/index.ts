01183c662310aeee7d22602eef0b2dcc7599f49c
GogumaYummy <sora1211x@gmail.com>
2023-01-09 10:49:57 +0900

    fix: 헬스 체크 응답에 상태 코드 추가

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,6 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req, res) => res.send('ok'));
+router.get('/', (req, res) => res.status(200).send('ok'));
 
 export default router;
