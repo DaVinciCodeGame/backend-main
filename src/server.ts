ba3a31d0de541a4a64c3cafcc079f424c27f936e
GogumaYummy <sora1211x@gmail.com>
2023-01-09 10:38:48 +0900

    fix: 포트 확인 로직 변경

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import App from './app';
 
 const app = new App();
 
-if (process.env.PORT) app.listen(Number(process.env.PORT));
-else throw new Error('No env: PORT');
+if (!process.env.PORT) throw new Error('유효하지 않은 환경 변수: PORT');
+app.listen(Number(process.env.PORT));
