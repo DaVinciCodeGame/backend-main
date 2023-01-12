639e2660d24e1baae7eb5d3a1911ad11f6bc261b
GogumaYummy <sora1211x@gmail.com>
2023-01-13 03:31:23 +0900

    feat: 유저 라우터 추가

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express';
 import authRouter from './auth.route';
+import userRouter from './users.route';
 
 const router = Router();
 
-router.use('/auth', authRouter).get('/', (req, res) => {
-  res
-    .links({ auth: `${req.protocol}://${req.hostname}/main/auth` })
-    .status(200)
-    .json('ok');
-});
+router
+  .get('/', (req, res) => {
+    res.status(200).json('ok');
+  })
+  .use('/auth', authRouter)
+  .use('/users', userRouter);
 
 export default router;
