1b06e69ee7f5e235fa9805ae0badd7c45c18a564
GogumaYummy <sora1211x@gmail.com>
2023-01-13 04:21:12 +0900

    fix: 누락된 미들웨어 추가

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import UsersController from '../controllers/users.controller';
+import authorize from '../middlewares/authorize';
 
 const usersRouter = Router();
 
 const usersController = new UsersController();
 
 usersRouter
-  .get('/me', usersController.getMyInfo)
+  .get('/me', authorize, usersController.getMyInfo)
   .get('/', usersController.getLeaderboard);
 
 export default usersRouter;
