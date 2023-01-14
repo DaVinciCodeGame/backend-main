48c508ded6787b937785db0887e91180e2b376a3
GogumaYummy <sora1211x@gmail.com>
2023-01-14 21:29:16 +0900

    fix: 응답 바디에서 토큰 제거

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -29,10 +29,7 @@ export default class AuthController {
           sameSite: 'none',
         })
         .status(isFirstTime ? 201 : 200)
-        .json({
-          message: isFirstTime ? '가입 완료' : '로그인 완료',
-          accessToken,
-        });
+        .json({ message: isFirstTime ? '가입 완료' : '로그인 완료' });
     } catch (err) {
       next(err);
     }
