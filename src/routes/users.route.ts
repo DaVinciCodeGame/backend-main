055e132ca982b0a16a1f30d0044fdca30ee71671
GogumaYummy <sora1211x@gmail.com>
2023-01-14 14:51:42 +0900

    feat: 멀티파트 파서 미들웨어 추가

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import UsersController from '../controllers/users.controller';
 import authorize from '../middlewares/authorize';
+import multipartParser from '../middlewares/multipart-parser';
 
 const usersRouter = Router();
 
@@ -9,6 +10,11 @@ const usersController = new UsersController();
 usersRouter
   .get('/me', authorize, usersController.getMyInfo)
   .get('/', usersController.getLeaderboard)
-  .put('/me', authorize, usersController.updateProfile);
+  .put('/me', authorize, usersController.updateProfile)
+  .post(
+    '/test',
+    multipartParser({ fileSize: 10 * 1024 * 1024 }).single('image'),
+    usersController.updateProfile
+  );
 
 export default usersRouter;
