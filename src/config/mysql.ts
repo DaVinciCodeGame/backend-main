7d0ba5501d81c94a4ca0cd660166d5ba32112c5f
GogumaYummy <sora1211x@gmail.com>
2023-01-25 18:10:55 +0900

    fix: MySQL 연결 최대 시간 설정

--- src/config/mysql.ts
+++ src/config/mysql.ts
@@ -8,6 +8,7 @@ const mysqlDataSource = new DataSource({
   url: env.MYSQL_URL,
   synchronize: env.NODE_ENV === 'development',
   entities: [User],
+  connectTimeout: 10000,
 });
 
 export default mysqlDataSource;
