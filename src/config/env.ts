8e0ecf3e3e6edcda977dec1b6a0598c7f3cf7288
GogumaYummy <sora1211x@gmail.com>
2023-01-29 04:36:18 +0900

    fix: 누락된 required 옵션 추가, 타입에 프로퍼티 명시

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,16 +3,22 @@ import Joi from 'joi';
 
 function validate() {
   const { value, error } = Joi.object<{
-    [key: string]: string;
+    PORT: string;
+    NODE_ENV: string;
+    MYSQL_URL: string;
+    KAKAO_REST_API_KEY: string;
+    JWT_SECRET: string;
+    AWS_S3_ACCESS_KEY_ID: string;
+    AWS_S3_SECRET_ACCESS_KEY: string;
   }>()
     .keys({
-      PORT: Joi.string(),
-      NODE_ENV: Joi.string(),
-      MYSQL_URL: Joi.string(),
-      KAKAO_REST_API_KEY: Joi.string(),
-      JWT_SECRET: Joi.string(),
-      AWS_S3_ACCESS_KEY_ID: Joi.string(),
-      AWS_S3_SECRET_ACCESS_KEY: Joi.string(),
+      PORT: Joi.string().required(),
+      NODE_ENV: Joi.string().required(),
+      MYSQL_URL: Joi.string().required(),
+      KAKAO_REST_API_KEY: Joi.string().required(),
+      JWT_SECRET: Joi.string().required(),
+      AWS_S3_ACCESS_KEY_ID: Joi.string().required(),
+      AWS_S3_SECRET_ACCESS_KEY: Joi.string().required(),
     })
     .unknown()
     .validate(process.env);
