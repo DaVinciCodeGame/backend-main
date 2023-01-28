c6b012d596419d0ff47245e4b92fe6dd25def349
GogumaYummy <sora1211x@gmail.com>
2023-01-29 06:18:23 +0900

    feat: 멀티파트 파싱 미들웨어 추가

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import UsersController from '../controllers/users.controller';
 import authorize from '../middlewares/authorize';
+import { single } from '../middlewares/multipart-parser';
 
 const usersRouter = Router();
 
@@ -9,7 +10,12 @@ const usersController = new UsersController();
 usersRouter
   .get('/me', authorize, usersController.getMyInfo)
   .get('/', usersController.getLeaderboard)
-  .put('/me', authorize, usersController.updateProfile)
+  .put(
+    '/me',
+    authorize,
+    single({ field: 'image', fileSize: 500 * 1024 }),
+    usersController.updateProfile
+  )
   .delete('/me', authorize, usersController.unregister);
 
 export default usersRouter;
