27200fe7e412def6680f7ecdaf38ebf37d5de211
GogumaYummy <sora1211x@gmail.com>
2023-01-29 03:46:52 +0900

    refactor: 환경 변수의 타입 가드 방식 변경

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,29 +1,27 @@
 import 'dotenv/config';
 import Joi from 'joi';
 
-type Env = {
-  PORT: string;
-  NODE_ENV: string;
-  MYSQL_URL: string;
-  KAKAO_REST_API_KEY: string;
-  JWT_SECRET: string;
-  AWS_S3_ACCESS_KEY_ID: string;
-  AWS_S3_SECRET_ACCESS_KEY: string;
-};
+function validate() {
+  const { value, error } = Joi.object<{
+    [key: string]: string;
+  }>()
+    .keys({
+      PORT: Joi.string(),
+      NODE_ENV: Joi.string(),
+      MYSQL_URL: Joi.string(),
+      KAKAO_REST_API_KEY: Joi.string(),
+      JWT_SECRET: Joi.string(),
+      AWS_S3_ACCESS_KEY_ID: Joi.string(),
+      AWS_S3_SECRET_ACCESS_KEY: Joi.string(),
+    })
+    .unknown()
+    .validate(process.env);
 
-const { value: env, error } = Joi.object<Env>()
-  .keys({
-    PORT: Joi.string(),
-    NODE_ENV: Joi.string(),
-    MYSQL_URL: Joi.string(),
-    KAKAO_REST_API_KEY: Joi.string(),
-    JWT_SECRET: Joi.string(),
-    AWS_S3_ACCESS_KEY_ID: Joi.string(),
-    AWS_S3_SECRET_ACCESS_KEY: Joi.string(),
-  })
-  .unknown()
-  .validate(process.env);
+  if (error) throw error;
 
-if (error) throw error;
+  return value;
+}
 
-export default env as Env;
+const env = validate();
+
+export default env;
