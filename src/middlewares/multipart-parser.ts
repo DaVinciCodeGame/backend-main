a3bd45863327010ce754cf9f68af812d0975bb01
GogumaYummy <sora1211x@gmail.com>
2023-01-29 01:57:09 +0900

    refactor: 미들웨어 생성자 함수로 변경

--- src/middlewares/multipart-parser.ts
+++ src/middlewares/multipart-parser.ts
@@ -2,23 +2,23 @@ import { badRequest } from '@hapi/boom';
 import { NextFunction, Request, Response } from 'express';
 import formidable from 'formidable';
 
-export default function multipartParser(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  try {
-    const form = formidable();
+export function single(fieldName: string) {
+  function multipartParser(req: Request, res: Response, next: NextFunction) {
+    try {
+      const form = formidable();
 
-    form.parse(req, (err, field, files) => {
-      if (err) throw err;
-      if (!files.image) throw badRequest('image 필드가 없습니다.');
+      form.parse(req, (err, field, files) => {
+        if (err) throw err;
+        if (!files[fieldName])
+          throw badRequest(`${fieldName} 필드가 없습니다.`);
 
-      res.locals.image = files.image;
+        res.locals.files[fieldName] = files[fieldName];
 
-      next();
-    });
-  } catch (err) {
-    next(err);
+        next();
+      });
+    } catch (err) {
+      next(err);
+    }
   }
+  return multipartParser;
 }
