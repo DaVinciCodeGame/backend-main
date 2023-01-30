bc6d1a76a49ddcaa604532b92b238a5ade6db4c4
GogumaYummy <sora1211x@gmail.com>
2023-01-30 19:01:29 +0900

    feat: 컨트롤러 연결

--- src/routes/rooms.route.ts
+++ src/routes/rooms.route.ts
@@ -1,6 +1,11 @@
 import { Router } from 'express';
+import RoomsController from '../controllers/rooms.controller';
 import authorize from '../middlewares/authorize';
 
 const roomsRouter = Router();
+
+const rommsController = new RoomsController();
+
+roomsRouter.post('/').get('/').post('/quickstart');
 
 export default roomsRouter;
