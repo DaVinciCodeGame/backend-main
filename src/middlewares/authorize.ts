d7596db81ba11ac3389a7bc06d9e6dc37590510d
GogumaYummy <sora1211x@gmail.com>
2023-01-31 19:28:23 +0900

    feat: 인증 미들웨어에서 res.locals에 토큰 유효시간 추가

--- src/middlewares/authorize.ts
+++ src/middlewares/authorize.ts
@@ -18,13 +18,18 @@ const authorize: RequestHandler = async (req, res, next) => {
       throw badRequest('인증 정보가 유효하지 않습니다.', '유효하지 않은 토큰');
     }
 
-    if (typeof payload === 'string' || typeof payload.userId !== 'number')
+    if (
+      typeof payload === 'string' ||
+      typeof payload.userId !== 'number' ||
+      !payload.exp
+    )
       throw badRequest(
         '인증 정보가 유효하지 않습니다.',
         '내용이 유효하지 않음'
       );
 
     res.locals.userId = payload.userId;
+    res.locals.accessTokenExp = payload.exp;
 
     next();
   } catch (err) {
