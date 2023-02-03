cb795e9ee98bf816d3e4245b9b43e53356c32293
GogumaYummy <sora1211x@gmail.com>
2023-02-03 19:50:44 +0900

    feat: 유저 정보 확인 api에 api 키 검증 미들웨어 추가

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import UsersController from '../controllers/users.controller';
 import authorize from '../middlewares/authorize';
 import { single } from '../middlewares/multipart-parser';
+import verifyApiKey from '../middlewares/verify-api-key';
 
 const usersRouter = Router();
 
@@ -9,6 +10,7 @@ const usersController = new UsersController();
 
 usersRouter
   .get('/me', authorize, usersController.readMyInfo)
+  .get('/:userId', verifyApiKey, usersController.read)
   .get('/', usersController.readMany)
   .put(
     '/me',
