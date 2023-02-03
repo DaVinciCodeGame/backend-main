fc03ad96e6ba3f6bf6fae19a93dfa21c69497098
GogumaYummy <sora1211x@gmail.com>
2023-02-03 19:38:12 +0900

    환경 변수 추가

--- src/config/env.ts
+++ src/config/env.ts
@@ -12,6 +12,7 @@ function validate() {
     AWS_S3_SECRET_ACCESS_KEY: string;
     AWS_S3_BUCKET_NAME: string;
     KAKAO_ADMIN_KEY: string;
+    API_KEY: string;
   }>()
     .keys({
       PORT: Joi.string().required(),
@@ -23,6 +24,7 @@ function validate() {
       AWS_S3_SECRET_ACCESS_KEY: Joi.string().required(),
       AWS_S3_BUCKET_NAME: Joi.string().required(),
       KAKAO_ADMIN_KEY: Joi.string().required(),
+      API_KEY: Joi.string().required(),
     })
     .unknown()
     .validate(process.env);
