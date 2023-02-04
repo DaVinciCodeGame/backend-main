8c471f48707cb7aed7adaea193b57594a641a50a
GogumaYummy <sora1211x@gmail.com>
2023-02-05 04:11:10 +0900

    refactor: 에러 메세지 명확하게 변경

--- src/middlewares/authorize.ts
+++ src/middlewares/authorize.ts
@@ -8,7 +8,8 @@ const authorize: RequestHandler = async (req, res, next) => {
   const { accessToken, refreshToken } = req.cookies;
 
   try {
-    if (!accessToken) throw unauthorized('인증 정보가 유효하지 않습니다.');
+    if (!accessToken)
+      throw unauthorized('쿠키에 엑세스 토큰이 포함되어 있지 않습니다.');
 
     let payload: string | jwt.JwtPayload | null;
 
@@ -16,7 +17,7 @@ const authorize: RequestHandler = async (req, res, next) => {
       payload = jwt.verify(accessToken, env.JWT_SECRET);
     } catch (err) {
       if (!(err instanceof TokenExpiredError)) {
-        throw unauthorized('인증 정보가 유효하지 않습니다.');
+        throw unauthorized('엑세스 토큰이 유효하지 않습니다.');
       }
 
       payload = jwt.decode(accessToken);
@@ -26,18 +27,18 @@ const authorize: RequestHandler = async (req, res, next) => {
         typeof payload === 'string' ||
         typeof payload.userId !== 'number'
       ) {
-        throw unauthorized('인증 정보가 유효하지 않습니다.');
+        throw unauthorized('엑세스 토큰의 내용이 유효하지 않습니다.');
       }
 
       const usersRepository = new UsersRepository();
       const user = await usersRepository.findOneByUserId(payload.userId);
 
       if (!user) {
-        throw unauthorized('인증 정보가 유효하지 않습니다.');
+        throw unauthorized('해당하는 유저 정보가 없습니다.');
       }
 
       if (user.refreshToken !== refreshToken) {
-        throw unauthorized('인증 정보가 유효하지 않습니다.');
+        throw unauthorized('리프레시 토큰이 유효하지 않습니다.');
       }
 
       const newAccessToken = jwt.sign(
@@ -62,7 +63,7 @@ const authorize: RequestHandler = async (req, res, next) => {
       typeof payload.userId !== 'number' ||
       !payload.exp
     )
-      throw unauthorized('인증 정보가 유효하지 않습니다.');
+      throw unauthorized('엑세스 토큰의 내용이 유효하지 않습니다.');
 
     res.locals.userId = payload.userId;
     res.locals.accessTokenExp = payload.exp * 1000 - Date.now();
